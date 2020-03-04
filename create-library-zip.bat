7f3ec6bf8292e247d82eb3ce63c984c5cdbace6e
fauresystems <34378695+fauresystems@users.noreply.github.com>
2020-03-04 08:52:51 +0100

    Update ArduinoProps.zip

--- create-library-zip.bat
+++ create-library-zip.bat
@@ -1,2 +1,2 @@
-;del ArduinoProps.zip
-7z.exe a -tzip ArduinoProps.zip -xr!*.zip -xr!*.git* -xr!*.bat -xr!.vs -xr!*.pdb -xr!*.sdf -xr!*.sln -xr!*.vcxitems -xr!*.vcxitems.filters ..\ArduinoProps
+
+C:\"Program Files"\7-Zip\7z.exe  a -tzip ArduinoProps.zip -xr!*.zip -xr!*.git* -xr!*.bat -xr!.vs -xr!*.pdb -xr!*.sdf -xr!*.sln -xr!*.vcxitems -xr!*.vcxitems.filters ..\ArduinoProps
